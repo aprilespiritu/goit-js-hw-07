36c6cf8496f3a8f2febbe58f215afb202489b94e
aprilespiritu <april_espiritu@live.com>
2024-04-14 21:25:27 +0800

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+let gallery = document.querySelector(".gallery");
+
+galleryItems.forEach((item) => {
+	let li = `
+    <li class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+        <img
+            class="gallery__image"
+            src="${item.preview}"
+            data-source="${item.original}"
+            alt="${item.description}"
+        />
+        </a>
+    </li>
+    `;
+	gallery.innerHTML += li;
+});
